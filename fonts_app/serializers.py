95d42b66196597523685ce40d9fba6560ac303d9
Dmitriy Alexandrov <newsmobileads@gmail.com>
2026-01-06 19:02:08 +0500

    Добавлено поле license_type_label

--- fonts_app/serializers.py
+++ fonts_app/serializers.py
@@ -5,16 +5,23 @@ from .models import FontFacePrice, FontFace
 class FontFaceSerializer(serializers.ModelSerializer):
     class Meta:
         model = FontFace
-        fields = '__all__'
+        fields = "__all__"
 
 
 class FontFacePriceSerializer(serializers.ModelSerializer):
     font_name = serializers.CharField(source="face.font.name", read_only=True)
     font_author = serializers.CharField(source="face.font.author", read_only=True)
-    font_date_release = serializers.CharField(source="face.font.date_release", read_only=True)
+    font_date_release = serializers.CharField(
+        source="face.font.date_release", read_only=True
+    )
     font_desc = serializers.CharField(source="face.font.desc", read_only=True)
 
     style_name = serializers.CharField(source="face.style.name", read_only=True)
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        data["license_type_label"] = instance.get_license_type_display()
+        return data
 
     class Meta:
         model = FontFacePrice
